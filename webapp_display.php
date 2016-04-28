8cba0e46b59eb43fb4bd1966e88520425d75bae3
motttey <motitago@gmail.com>
2016-04-28 23:15:27 +0900

    Update webapp_display.php

--- webapp_display.php
+++ webapp_display.php
@@ -4,7 +4,7 @@
 <body>
 <?php
 //データベースへの接続
-$mysqli = new mysqli("127.0.0.1", "root", "0718", "webapp");
+$mysqli = new mysqli("127.0.0.1", "root", "password", "webapp");
 if (!$mysqli) {
 	exit('データベースに接続できませんでした。');
 }
